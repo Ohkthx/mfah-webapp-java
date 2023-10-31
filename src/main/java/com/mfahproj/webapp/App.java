51086a6615b6a347d8f3d6bb59522a7cce450086
Ohkthx <11020006+Ohkthx@users.noreply.github.com>
2023-10-31 16:35:12 -0500

    Converted callback into function to cleanup Main.

--- src/main/java/com/mfahproj/webapp/App.java
+++ src/main/java/com/mfahproj/webapp/App.java
@@ -8,9 +8,52 @@ import com.sun.net.httpserver.HttpServer;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // This is processed before each request is made. Returns true if redirect
-        // happened.
-        MiddlewareHandler.HttpRequestCallback callback = exchange -> {
+        // Load the configuration file.
+        Properties config = Utils.loadConfig("app.config");
+        int port = 8080;
+        try {
+            port = Integer.parseInt(config.getProperty("webapp.port"));
+        } catch (Exception e) {
+            System.out.println("Unable to parse the webapp port number, check configuration file.");
+            System.exit(1);
+        }
+
+        // Set the database variables.
+        Database.setConfiguration(config);
+
+        // Create a thread responsible for just purging expired sessions.
+        Session.startScheduler();
+
+        // Create the HTTP server and URIs to handle.
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        MiddlewareHandler.HttpRequestCallback callback = App.createCallback();
+
+        // Homepage
+        server.createContext("/", new MiddlewareHandler(new HomeHandler(), callback));
+        server.createContext("/not-implemented", new MiddlewareHandler(new NotImplementedHandler(), callback));
+
+        // Login / Logout Page
+        server.createContext("/login", new MiddlewareHandler(new LoginHandler(), callback));
+        server.createContext("/logout", new MiddlewareHandler(new LogoutHandler(), callback));
+
+        // Homepages
+        server.createContext("/member", new MiddlewareHandler(new MemberHandler(), callback));
+        server.createContext("/employee", new MiddlewareHandler(new EmployeeHandler(), callback));
+
+        // Used for registeration.
+        server.createContext("/register", new MiddlewareHandler(new RegisterMemberHandler(), callback));
+        server.createContext("/register-employee", new MiddlewareHandler(new RegisterEmployeeHandler(), callback));
+        server.setExecutor(null);
+
+        // Start the server for listening.
+        server.start();
+    }
+
+    // Creates a callback that is used to serve as middleware between requests to
+    // the server by clients. This handles sending them to the timeout page and
+    // removing their session locally.
+    public static MiddlewareHandler.HttpRequestCallback createCallback() {
+        return exchange -> {
             String sessionId = Session.extractSessionId(exchange);
             if (sessionId == null || !Session.exists(sessionId)) {
                 return false;
@@ -32,44 +75,5 @@ public class App {
 
             return false;
         };
-
-        // Load the configuration file.
-        Properties config = Utils.loadConfig("app.config");
-        int port = 8080;
-        try {
-            port = Integer.parseInt(config.getProperty("webapp.port"));
-        } catch (Exception e) {
-            System.out.println("Unable to parse the webapp port number, check configuration file.");
-            System.exit(1);
-        }
-
-        // Set the database variables.
-        Database.setConfiguration(config);
-
-        // Create a thread responsible for just purging expired sessions.
-        Session.startScheduler();
-
-        // Create the HTTP server and URIs to handle.
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
-        // Homepage
-        server.createContext("/", new MiddlewareHandler(new HomeHandler(), callback));
-        server.createContext("/not-implemented", new MiddlewareHandler(new NotImplementedHandler(), callback));
-
-        // Login / Logout Page
-        server.createContext("/login", new MiddlewareHandler(new LoginHandler(), callback));
-        server.createContext("/logout", new MiddlewareHandler(new LogoutHandler(), callback));
-
-        // Homepages
-        server.createContext("/member", new MiddlewareHandler(new MemberHandler(), callback));
-        server.createContext("/employee", new MiddlewareHandler(new EmployeeHandler(), callback));
-
-        // Used for registeration.
-        server.createContext("/register", new MiddlewareHandler(new RegisterMemberHandler(), callback));
-        server.createContext("/register-employee", new MiddlewareHandler(new RegisterEmployeeHandler(), callback));
-        server.setExecutor(null);
-
-        // Start the server for listening.
-        server.start();
     }
 }
